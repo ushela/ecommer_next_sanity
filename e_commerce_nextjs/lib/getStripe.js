32b7d747cc6b91a9629d1277073d839a382b93f1
Ushie Ekunke <ekunkeushie@gmail.com>
2022-06-06 13:56:23 +0100

    Added my project

---
+++ e_commerce_nextjs/lib/getStripe.js
@@ -0,0 +1,14 @@
+import { loadStripe } from '@stripe/stripe-js';
+
+let stripePromise;
+
+
+const getStripe = () => {
+    // if the stripe promise doesnt yet exist(!stripepromise)load stripePromise
+    if(!stripePromise) {
+        stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY);
+    }
+    return stripePromise;
+}
+
+export default getStripe; 
